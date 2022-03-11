689bed84c645470ef0a9a0baa99c3a67af9e57f2
Antra <antra.kacena@gmail.com>
2022-03-11 21:05:21 +0200

    Added everything

---
+++ app/src/main/java/com/antra_kacena/apitest/MainActivity.java
@@ -0,0 +1,53 @@
+package com.antra_kacena.apitest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+import com.android.volley.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        RequestQueue queue = Volley.newRequestQueue(this);
+        String url = "http://meowfacts.herokuapp.com/";
+
+        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.i("Praktiskais", "response:" + response.toString());
+                try {
+                    JSONArray fact = response.getJSONArray("data");
+
+                } catch (Exception e) {
+                    Log.i("Praktiskais", "error:" + e.toString());
+                }
+
+               // adapter.notifyDataSetChanged();
+            }
+        }, new Response.ErrorListener() {
+
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.i("Praktiskais", "error:" + error.toString());
+            }
+        });
+    }
+}
